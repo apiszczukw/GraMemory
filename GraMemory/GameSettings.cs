e04bc8765b926e79abcac5076e2be21789591cf0
Adrianna Piszcz <adrianna.piszcz@vp.pl>
2021-03-27 12:56:51 +0100

    Dodanie startowych ustawień gry

--- GraMemory/GameSettings.cs
+++ GraMemory/GameSettings.cs
@@ -8,5 +8,31 @@ namespace GraMemory
 {
     class GameSettings
     {
+        public int CzasGry;
+        public int CzasPodgladu;
+        public int MaxPunkty;
+        public int Wiersze;
+        public int Kolumny;
+        public int Bok;
+        public int AktualnePunkty;
+
+        public string PlikLogo = $"{AppDomain.CurrentDomain.BaseDirectory}\\Obrazki\\logo.jpg";
+        public string FolderObrazki = $"{AppDomain.CurrentDomain.BaseDirectory}\\Obrazki\\Karty";
+
+        public GameSettings()
+        {
+            UstawienieStartowe();
+        }
+
+        private void UstawienieStartowe()
+        {
+            CzasPodgladu = 5;
+            CzasGry = 60;
+            MaxPunkty = 0;
+            Wiersze = 4;
+            Kolumny = 6;
+            Bok = 150;
+            AktualnePunkty = 0;
+        }
     }
 }
