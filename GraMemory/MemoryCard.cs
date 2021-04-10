9bc25d177df1266489091a70c9e7ac9a8590bb72
Adrianna Piszcz <adrianna.piszcz@vp.pl>
2021-04-10 12:26:26 +0200

    Dodanie klasy karty, pól klasy i kontruktora

---
+++ GraMemory/MemoryCard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraMemory
+{
+    class MemoryCard : Label
+    {
+        public Guid ID;
+        public Image Front;
+        public Image Back;
+
+        public MemoryCard(Guid iD, Image front, Image back)
+        {
+            ID = iD;
+            Front = front;
+            Back = back;
+            BackgroundImageLayout = ImageLayout.Stretch;
+        }
+    }
+}
